616d6ef1241bd90cf824b8cb5b9dcb8883cd94ca
Neeraj Kumar Sharma <neerajkumar.kumar933@gmail.com>
2017-09-30 00:35:48 +0530

    Update PIR.cpp
    added comments

--- PIR.cpp
+++ PIR.cpp
@@ -1,14 +1,16 @@
+/*This program is used to test the PIR sensor function with the mbed board*/
+
 #include "mbed.h"
-DigitalIn pir(D7);
-DigitalOut myled(LED1);
+DigitalIn pir(D7);          //data is collected at pin D7 of the mbed board from PIR sensor
+DigitalOut myled(LED1);     //an indicator whether there is a motion or not
 
 int main() {
   while(1) {
-    if (pir == 1) {
-      myled = 0;
+    if (pir == 1) {         //checks whether the motion of a person is true
+      myled = 0;            //led glows if the condition is true
     }
-    else if (pir == 0) {
-      myled = 1;
+    else if (pir == 0) {    //checks whether the motion of a person is false
+      myled = 1;            //led goes off if the condition is true
     }
   }
 }
